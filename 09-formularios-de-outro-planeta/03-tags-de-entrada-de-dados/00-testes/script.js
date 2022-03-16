0d37dd9b68a809873f24d52cbf8d9f90cdc003c5
Carlosjw <carlos.ti.infor@gmail.com>
2022-03-16 13:46:22 -0300

    litleChanges

--- 09-formularios-de-outro-planeta/03-tags-de-entrada-de-dados/00-testes/script.js
+++ 09-formularios-de-outro-planeta/03-tags-de-entrada-de-dados/00-testes/script.js
@@ -8,7 +8,9 @@ let btnSubtrair = document.getElementById("subtrair");
 let teste1 = document.getElementById("teste01");
 let teste2 = document.getElementById("teste02");
 let teste3 = document.getElementById("somas-numbers");
-let operacao = document.getElementById("operation")
+let teste5 = document.getElementById("teste03");
+let teste6 = document.getElementById("teste04");
+let teste4 = document.getElementById("sub-numbers");
 
 number01.oninput = function(event){
     teste1.innerHTML = number01.value;
@@ -18,10 +20,18 @@ number02.oninput = function(event){
     teste2.innerHTML = number02.value;
 }
 
+number01.oninput = function(event){
+    teste5.innerHTML = number01.value;
+}
+
+number02.oninput = function(event){
+    teste6.innerHTML = number02.value;
+}
+
 btnSomar.addEventListener('click', function(){
     teste3.innerHTML = parseInt(number01.value) + parseInt(number02.value);
 })
 
 btnSubtrair.addEventListener('click', function(){
-    teste3.innerHTML = parseInt(number01.value) - parseInt(number02.value);
+    teste4.innerHTML = parseInt(number01.value) - parseInt(number02.value);
 })
