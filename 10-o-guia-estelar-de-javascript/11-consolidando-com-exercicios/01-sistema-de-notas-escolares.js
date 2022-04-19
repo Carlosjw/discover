6f503975bf514ed9d5fb18443d7cda720b73dcbb
Carlosjw <carlos.ti.infor@gmail.com>
2022-04-19 15:55:33 -0300

    firstExercise

--- 10-o-guia-estelar-de-javascript/11-consolidando-com-exercicios/01-sistema-de-notas-escolares.js
+++ 10-o-guia-estelar-de-javascript/11-consolidando-com-exercicios/01-sistema-de-notas-escolares.js
@@ -11,3 +11,23 @@
     * menor que 60      -   F
      
 */
+
+function convertNota(nota){
+
+    if(nota >= 90){
+        nota = 'A';
+    }else if(nota >= 80 && nota <= 89){
+        nota = 'B'
+    }else if(nota >= 70 && nota <= 79){
+        nota = 'C'
+    }else if(nota >= 60 && nota <= 69){
+        nota = 'D'
+    }else {
+        nota = 'F'
+    }
+    
+    console.log(`Nota => "${nota}"`);
+}
+
+convertNota(50);
+
