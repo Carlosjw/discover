73af273cc7670e32e10240284f75f77f75631be9
Carlos Lima <68574705+Carlosjw@users.noreply.github.com>
2022-05-18 17:05:40 -0300

    updateLesson

--- 14-pilotando-com-a-DOM/08-praticando/desafio-modal/script.js
+++ 14-pilotando-com-a-DOM/08-praticando/desafio-modal/script.js
@@ -1,9 +1,14 @@
 const btn = document.querySelector('#openModal');
 const modalWrapper = document.querySelector('.modal-wrapper');
 const body = document.querySelector('body');
+const purpleDiv = document.querySelector('#purple-div');
 
 btn.addEventListener('click', function(){
     modalWrapper.style.visibility = 'visible';
+});
+
+purpleDiv.addEventListener('click', function(){
+  modalWrapper.style.visibility = 'hidden';
 });
 
 body.KeyboardEvent('keyup', function(){
