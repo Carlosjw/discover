596450f9a53ffd752ce9e4656bfaab9d5ee7864a
Carlosjw <carlos.ti.infor@gmail.com>
2022-05-27 00:42:16 -0300

    nodeLessons

---
+++ 15-node-o-motor-da-nave/03-iniciando-na-prática/05-passando-flags-como-argumento/process.js
@@ -0,0 +1,8 @@
+// lista de argumentos
+console.log(process.argv);
+
+/* 
+    Podemos passar os argumentos como flags diretamente do Node:
+    Ex:
+        node process --name "Carlos" --greeting "Tudo bem como você?"
+*/
