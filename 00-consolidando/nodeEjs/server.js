d603c48e0a0e5b46c17422f4544cf38490730121
Carlosjw <carlos.ti.infor@gmail.com>
2022-06-18 00:22:02 -0300

    endProject

--- 00-consolidando/nodeEjs/server.js
+++ 00-consolidando/nodeEjs/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+
+// definindo caminho do css
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
@@ -9,7 +11,11 @@ app.get("/", function(req, res){
 });
 
 app.get("/sobre", function(req, res){
-    res.render("about");
+    res.render("pages/info");
+})
+
+app.get("/contactUs", function(req, res){
+    res.render("pages/contactUs")
 })
 
 app.listen(8080);
