0236b9efe76f6274b5d6509ba5dd3fdd26175c82
Carlosjw <carlos.ti.infor@gmail.com>
2022-06-20 01:59:35 -0300

    updateNodeProject

--- 00-consolidando/sampleNodeJS/server.js
+++ 00-consolidando/sampleNodeJS/server.js
@@ -8,7 +8,19 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
     res.render('pages/index');
-})
+});
+
+app.get("/register", function(req, res){
+    res.render('pages/register');
+});
+
+app.get('/reports', function(req, res){
+    res.render('pages/reports');
+});
+
+app.get('/login', function(req, res){
+    res.render('pages/login');
+});
 
 app.listen(8080);
-console.log('Server is running...')
+console.log('Server is running...');
