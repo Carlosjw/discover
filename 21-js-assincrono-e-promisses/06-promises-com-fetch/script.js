81cfabdcba6fb9566099aca47570f00886f1c5ad
Carlosjw <carlos.ti.infor@gmail.com>
2022-07-20 23:29:32 -0300

    promisesLessonUpdate

--- 21-js-assincrono-e-promisses/06-promises-com-fetch/script.js
+++ 21-js-assincrono-e-promisses/06-promises-com-fetch/script.js
@@ -1,5 +1,8 @@
 // inserir código no Console do Chrome
 
-fetch('https://api.github.com/users/Carlosjw').then(response => {
-    response.json().then(data => console.log(data))
-});
+fetch('https://api.github.com/users/Carlosjw')
+.then(response => response.json())
+.then(data => fetch(data.repos_url))
+.then(res => res.json())
+.then(d => console.log(d))
+.catch(err => console.log(err))
