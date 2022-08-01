a6ed21ae0011fcc44786aa27ed9186a9b163bccd
Carlosjw <carlos.ti.infor@gmail.com>
2022-08-01 00:06:44 -0300

    updateLesson

---
+++ 22-trabalhando-com-APIs/03-metodos-http/README.md
@@ -0,0 +1,23 @@
+# Métodos HTTP
+
+## `GET`
+
+    * A API fornece as informações
+    * Ex:
+      * Quando digitamos o link de um site no navegador, estamos fazendo um GET e a API retorna um site navegável no Brownser
+
+## `POST`
+
+    * A API recebe informações que devem ou não ser registradas
+
+## `DELETE`
+
+    * A API recebe identificadores de registros que devem ser apagados
+
+## `PUT`
+
+    * A API recebe informações para update de um ou mais registros
+
+## `PATCH`
+
+    * A API recebe informações para update de UM registro
---
+++ 22-trabalhando-com-APIs/05-iniciando-projeto-em-NodeJS/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express()
+
+app.listen('3000');
+
+/* rota GET
+app.route('/').get((req, res) => res.send('Hello'))
+app.route('/sobre').get((req, res) => res.send('Hello sobre')) */
+
+/* 
+    / => define a rota padrão base/inicial ou link inicial do site
+    get((req, res)) => sempre usados quando formos trabalhar com rotas, significa 'requisição' e 'response'
+*/
+
+/* // rota POST
+app.use(express.json()) // middleware => fazer a ponte quando for usar o Insomnia
+app.route('/').post((req, res) => res.send (req.body)) */
+
+app.use(express.json()) // middleware
+
+// rota PUT: editar
+
+/* let author = 'Carlos'
+
+app.route('/').get((req, res) => res.send(author));
+
+app.route('/').put((req, res) =>{
+    author = req.body.author // recebe apenas o valor da propriedade author do JSON
+    res.send(author)
+}); */
+
+// rota DELETE
+let author = "Carlos Lima"
+
+app.route('/:identificador').delete((req, res) => {
+    res.send(req.params.identificador)
+})
+
+// colocar no Insomnia "localhost:3000/0101"
+/* 
+    Normalmente o DELETE recebe o identidicador na ROTA
+    Os dois pontos (:) indicam que o identificado é uma variável que possui conteúdo. Quando passamos uma variável pela rota, chamamos isso de 'params'
+*/
