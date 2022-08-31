c831c8b2f4961e5fa3d96443f79c2c89170df5bf
Carlosjw <carlos.ti.infor@gmail.com>
2022-08-31 15:22:53 -0300

    updateLesson

---
+++ 00-consolidando/blog-simple/js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+    pagination: {
+        el: '.blog-slider_pagination',
+        clickable: true,
+    }
+})
