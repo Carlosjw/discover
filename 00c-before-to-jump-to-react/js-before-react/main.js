b469fbae2571b861cf441e317dde1e7d4b6de17e
Carlosjw <carlos.ti.infor@gmail.com>
2022-09-15 14:37:14 -0300

    litleChanges

--- 00c-before-to-jump-to-react/js-before-react/main.js
+++ 00c-before-to-jump-to-react/js-before-react/main.js
@@ -1,15 +1,15 @@
 // importando uma por uma
-/* import { sum, subtrair } from './lib/math'
+import { sum, subtrair } from './lib/math'
 
 document.body.innerText = sum(1, 2);
-document.body.innerText = subtrair(7, 2); */
+document.body.innerText = subtrair(7, 2);
 
 // importando tudo
-/* import * as math from './lib/math'
+import * as math from './lib/math'
 
 console.log(math)
 document.body.innerText = math.sum(3, 6);
-document.body.innerText = math.sub(3, 6); */
+document.body.innerText = math.sub(3, 6);
 
 // importando e renomeando
 import { sum as soma } from './lib/math'
