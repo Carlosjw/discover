3d3da50fdac03f13520048c7829a5e1a3ed713cc
Carlosjw <carlos.ti.infor@gmail.com>
2022-09-26 15:51:43 -0300

    updatedLessons

--- 22-trabalhando-com-APIs/08-API-no-Front-End-com-Fetch/fetch-api-discover/main.js
+++ 22-trabalhando-com-APIs/08-API-no-Front-End-com-Fetch/fetch-api-discover/main.js
@@ -1,27 +1,35 @@
-const url = "http://localhost:5500/api"
+
+const url = 'http://localhost:5500/api';
 
 function getUsers(){
     fetch(url)
+    // se tudo der certo
     .then(response => response.json())
-    .then(data => renderApiResult.textContent = JSON.stringify(data)) // renderizando no html
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    // se der errado
     .catch(error => console.error(error));
 }
 
+// pegando um único usuário
 function getUser(){
-    fetch(`${url}/13`)
+    fetch(`${url}/1`) // acessando usuário pelo ID
     .then(response => response.json())
     .then(data => {
-        userName.textContent = `Nome: ${data.name}`
-        userCity.textContent = `Cidade: ${data.city}`
-        userAvatar.src = data.avatar
+        // chamando DOM direto
+        userName.textContent = data.name;
+        userCity.textContent = data.userCity;
+        // renderizando imagem
+        userAvatar.src = data.avatar;
+
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
 }
 
+// POST
 function addUser(newUser){
-    fetch(url, {
+    fetch(url,{
         method: "POST",
-        body: JSON.stringify(newUser), // transformando em JSON
+        body: JSON.stringify(newUser),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
@@ -29,15 +37,37 @@ function addUser(newUser){
     .then(response => response.json())
     .then(data => alertApi.textContent = data)
     .catch(error => console.error(error))
+
+}
+
+// PUT
+function updateUser(updatedUser){
+    fetch(`${url}/1`, {
+        method: "PUT",
+        body: JSON.stringify(updatedUser),
+        headers: {
+        "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.log(error))
 }
 
 const newUser = {
-    name: "Olivia Zars",
-    avatar: "https://picsum.photos/200/300",
-    city: "Rio do Sul"
+    name: 'Carlos Lima',
+    avatar: 'https://picsum.photos/200/300',
+    city: 'Rio do Sul'
 }
 
-addUser(newUser)
+// PUT
+const updatedUser = {
+    name: "Solange Lima",
+    avatar: "images/solange.jpg",
+    city: 'Marabá'
+}
+
+// addUser(newUser)
 
 getUsers()
 getUser()
