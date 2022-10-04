557e9dfad02bb2ad3558fa9e82d931553c769eb4
¨Carlosjw¨ <carlos.ti.infor@gmail.com>
2022-10-04 16:27:28 -0300

    updateLesson

--- 23-Fundamentos-do-React/firstApp/src/pages/Home/index.jsx
+++ 23-Fundamentos-do-React/firstApp/src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ import { Card } from '../../components/Card';
 
 export function Home() {
   /* Utilizando o estado */
-  const [studentName, setStudentName] = useState();
+  const [studentName, setStudentName] = useState('');
   /* 
     studentName = conteúdo do estado
     setStudentName = função que atualiza o estado
@@ -20,6 +20,7 @@ export function Home() {
   function handleAddStudent(){
     const newStudent = {
       name: studentName,
+      // acessando data e hora atuais e renderizando no padrão brasileiro
       time: new Date().toLocaleTimeString("pt-br", {
         hour: '2-digit',
         minute: '2-digit',
@@ -27,7 +28,8 @@ export function Home() {
       })
     };
 
-    setStudents(prevState => [prevState, newStudent])
+    // adicionando objeto ao estado e mantendo o conteúdo o estado anterior com o spred operator dentro da variável prevState
+    setStudents(prevState => [...prevState, newStudent])
 
   }
 
@@ -39,13 +41,20 @@ export function Home() {
         type="text"
         placeholder="Digite o nome..."
         /* capturando valor o input e atualizando variável studentName */
-        onChange={e => setStudentName(e.target.value)}
+        onChange={(e)=> setStudentName(e.target.value)}
       />
-      <button type="button" onClick={handleAddStudent}>Adicionar</button>
+      <button
+        type="button"
+        onClick={handleAddStudent}>
+          Adicionar
+      </button>
 
       {/* Passando propriedades pro componente */}
+      {/* Envolver por chaves indica que vamos usar conteúdo de uma variável */}
       {
-        students.map(student => <Card name={student.name} time={student.time} />)
+        students.map((student) => (
+          <Card name={student.name} time={student.time} />))
+        // Para cada estudante será renderizado um cartão
       }
 {/*       <Card name="Solange" time="11:00:10"/>
       <Card name="Saymon" time="16:15:25"/> */}
