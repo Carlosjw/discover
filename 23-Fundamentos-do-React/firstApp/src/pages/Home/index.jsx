3dd0b2eb9de278b239fa72913208c49f2643de9e
Carlosjw <carlos.ti.infor@gmail.com>
2022-10-03 23:04:43 -0300

    updateLesson

--- 23-Fundamentos-do-React/firstApp/src/pages/Home/index.jsx
+++ 23-Fundamentos-do-React/firstApp/src/pages/Home/index.jsx
@@ -1,30 +1,54 @@
+/* importando hook para criação de estados */
+import React,  {useState} from 'react';
+
 import './styles.css';
 
 // importando componente
 import { Card } from '../../components/Card';
+/* import { useState } from 'react'; */
 
 export function Home() {
+  /* Utilizando o estado */
+  const [studentName, setStudentName] = useState();
+  /* 
+    studentName = conteúdo do estado
+    setStudentName = função que atualiza o estado
+  */
 
-  function handleNameChange(name){
-    console.log(name);
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    };
+
+    setStudents(prevState => [prevState, newStudent])
+
   }
-
 
   return (
     // Embrulhando tudo no fragment
     <div className="container">
-      <h1>Lista de Presença</h1>
+      <h1>Lista de presença</h1>
       <input
         type="text"
         placeholder="Digite o nome..."
-        onChange={e => handleNameChange(e.target.value)}
+        /* capturando valor o input e atualizando variável studentName */
+        onChange={e => setStudentName(e.target.value)}
       />
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={handleAddStudent}>Adicionar</button>
 
       {/* Passando propriedades pro componente */}
-      <Card name="Carlos" time="10:55:25"/>
-      <Card name="Solange" time="11:00:10"/>
-      <Card name="Saymon" time="16:15:25"/>
+      {
+        students.map(student => <Card name={student.name} time={student.time} />)
+      }
+{/*       <Card name="Solange" time="11:00:10"/>
+      <Card name="Saymon" time="16:15:25"/> */}
     </div>
     // Também poderíamos embrulhar tudo em uma DIV
   )
