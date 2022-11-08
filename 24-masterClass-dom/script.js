a693e719186485d6296501626597e1f7a7263f29
Carlosjw <carlos.ti.infor@gmail.com>
2022-11-08 15:11:56 -0300

    updateLesson

--- 24-masterClass-dom/script.js
+++ 24-masterClass-dom/script.js
@@ -1,8 +1,9 @@
- let target = document.querySelector("#target");
+let target = document.querySelector("#target");
 let btn = document.querySelector("#btn");
 let elementos = document.querySelectorAll('.lista');
-let inputs = document.querySelectorAll('input');
-let catchInputs = document.querySelector
+let targetElements = document.querySelectorAll('.new-lista');
+
+let paragraph = document.querySelector("p");
 
 console.log(document)
 /* 
@@ -16,11 +17,45 @@ console.dir(document)
 console.log(document.title) // exibe o título da pagina
 
 elementos.forEach((element, index) => {
-    target.innerHTML += `${index + 1} => ${element.textContent} ${"</br>"}`
+    target.innerHTML += `${index + 1} => ${element.textContent} ${"</br>"}`   
+})
+
+let family = ["Núbia", "Bambú", "Heldin", "Deleon", "Ruth", "Carlos", "Manoel", "Joelma", "Suely", "Solange"]
+
+function random(number) {
+  return Math.floor(Math.random()*(number+1));
+}
+
+let randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+
+targetElements.forEach((targetElement, index )=> {
+    targetElements[index].innerText += family[index] + " Lima";
+    targetElements[index].style.padding = "10px"
+    targetElements[index].style.display = "flex"
+    targetElements[index].style.alignItems = "center"    
+    targetElements[index].style.justifyContent = "center"
+    targetElements[index].style.width = "100%";
+    targetElements[index].style.marginBottom = "5px";
+    targetElements[index].style.borderRadius = "4px";
+    targetElements[index].style.backgroundColor = "skyblue";
+    targetElements[index].style.boxShadow = "inset 2px 3px 6px rgba(0, 0, 0, 0.5)";
+
 })
 
 console.log(elementos)
 
-elementos[0].innerHTML = "<h1>Sou o zero mas sou o um</h1>"
+console.log(targetElements)
 
-catchInputs.addEventListeners()
+console.log(Math.random())
+
+btn.onclick = function() {
+    if(paragraph.style.display == 'flex'){
+        paragraph.style.display = 'none';
+    } else {
+        paragraph.style.display = 'flex'
+        paragraph.style.padding = "20px";
+        paragraph.style.borderRadius = "5px";
+        paragraph.style.backgroundColor = "skyblue";
+        paragraph.style.boxShadow = "inset 2px 3px 6px rgba(0, 0, 0, 0.5)";
+    }
+}
