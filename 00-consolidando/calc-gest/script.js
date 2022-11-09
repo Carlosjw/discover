ef3efff988987113cb61af42879a44733bbc6d75
Carlos Antonio de Moura Lima <68574705+Carlosjw@users.noreply.github.com>
2022-11-09 18:28:15 -0300

    update script.js

--- 00-consolidando/calc-gest/script.js
+++ 00-consolidando/calc-gest/script.js
@@ -16,7 +16,7 @@ let userData = document.querySelector("#qtd");
 let dayData = document.querySelector("#dayData");
 
 btn.addEventListener("click", function(){
-  dayData.textContent = userData.values;
+  dayData.textContent = userData.value;
   console.log(userData)
 })
 
