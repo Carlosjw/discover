d7c0f09fdd9cdc59d7f82b5ebf5dc52b00f56782
Carlos Antonio de Moura Lima <68574705+Carlosjw@users.noreply.github.com>
2022-11-09 18:15:16 -0300

    updateProject

--- 00-consolidando/calc-gest/script.js
+++ 00-consolidando/calc-gest/script.js
@@ -6,7 +6,12 @@ let thirdWeek = 1000;
 let fourthWeek = 10000;
 let secondAndThirdMonth = 100000;
 let secondQuarter = 30000;
+let thirdQuarter = 15000;
 
+// Pegando valor do input
+let userData = document.querySelector("#qtd").value;
+// Pegando campo dia alvo
+let dayData = document.querySelector("#dayData");
 
 
 /* 
