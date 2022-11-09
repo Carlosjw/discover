01aa59b260fc2993f05751181a366f3faf19973b
Carlos Antonio de Moura Lima <68574705+Carlosjw@users.noreply.github.com>
2022-11-09 18:09:18 -0300

    update script.js

--- 00-consolidando/calc-gest/script.js
+++ 00-consolidando/calc-gest/script.js
@@ -1,6 +1,12 @@
 let week = 7;
 
 let firstWeek = 30;
+let secondWeek = 100;
+let thirdWeek = 1000;
+let fourthWeek = 10000;
+let secondAndThirdMonth = 100000;
+let secondQuarter = 30000;
+
 
 
 /* 
