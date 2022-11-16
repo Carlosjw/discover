65700371a33be046e23430dbbe5ebda916cef1be
Carlosjw <carlos.ti.infor@gmail.com>
2022-11-16 15:16:56 -0300

    updateLessonProject

--- 25-masterClass-dom/project-piano/main.js
+++ 25-masterClass-dom/project-piano/main.js
@@ -6,9 +6,42 @@
 */
 // get all keys
 const keys = document.querySelectorAll(".key");
-console.log(keys)
+
 // play notes
+function playNote(event){
+    // console.log(event.target.dataset.key)
+
+    let audioKeyCode = getKeyCode(event)
+
+    // console.log(event.type); // tipo de evento
+    // console.log(audioKeyCode);
+
+    // if key exists
+
+    // play audio
+
+}
+
+function getKeyCode(event){
+    // keyCode
+    let keyCode;
+
+    const isKeyBoard = event.type === "keydown";
+    // typed or pressed key
+    if(isKeyBoard){
+        keyCode = event.keyCode;
+    } else {
+        keyCode = event.target.dataset.key;
+    }
+}
 
 // click with mouse
+keys.forEach(key =>{
+    // console.log(key.dataset.note) // exibindo a nota da tecla
+    key.addEventListener("click", playNote)
+})
 
 // keyboard types
+// adicionando evento de pressionamento de tecla
+// elemento window representa a janela de visualizaçõo
+window.addEventListener("keydown", playNote)
