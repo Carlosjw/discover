b6d062e9f569de7b62b6e28b135ac6855c1e8121
Carlosjw <carlos.ti.infor@gmail.com>
2022-11-28 12:13:34 -0300

    updateLessons

--- 00-consolidando/atendimentos/script.js
+++ 00-consolidando/atendimentos/script.js
@@ -1,11 +1,37 @@
-let month = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+let dayWeek = document.getElementById("week-day");
+
+let day = document.querySelector("#day");
+let month = document.querySelector("#month");
+let periodo = document.querySelector("#periodo");
+let updateHours = document.querySelector("#update-hours");
+
+let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
 let week = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
 
-let d = new Date();
+let data = new Date();
 
-document.getElementById("week-day").innerHTML = week[d.getDay()];
+setInterval(()=>{
+    //declaranda a data no escopo do intervalo
+    let data = new Date();
 
+    // pegando dia da semana
+    dayWeek.innerHTML = week[data.getDay()];
 
-let data = new Date()
-console.log(data.toLocaleString('pt-BR', {weekday: 'long'}))
+    // pegando dia do mês
+    day.textContent = data.toLocaleString("pt-BR", {day:"numeric"})
+
+    // pegando mês
+    month.textContent = months[data.getMonth()];
+
+    // verificando se é manhã ou tarde
+    if(data.getHours() >= 5 && data.getHours() < 12){
+        periodo.textContent = "Bom dia! ";
+    } else if(data.getHours() >= 12 && data.getHours() < 18){
+        periodo.textContent = "Boa tarde! "
+    }
+
+    // rodando hora em tempo real
+    updateHours.textContent = data.toLocaleTimeString('pt-BR'); 
+
+}, 1000);
